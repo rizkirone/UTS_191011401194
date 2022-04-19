553155be210c6b9d6f22d1ebb81b1cb7d6345e29
rizkirone <102182778+rizkirone@users.noreply.github.com>
2022-04-19 13:18:08 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
           crossAxisCount: 3,
           children: [
             Container(
-              padding: const EdgeInsets.all(45),
+              padding: const EdgeInsets.all(20),
               child: const Align(
                 alignment: Alignment.topLeft,
                 child: Text('One', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
               color: const Color.fromARGB(255, 163, 199, 199),
             ),
             Container(
-              padding: const EdgeInsets.all(45),
+              padding: const EdgeInsets.all(20),
               child: const Align(
                 alignment: Alignment.topLeft,
                 child: Text('Two',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
               color: const Color.fromARGB(255, 133, 169, 185),
             ),
             Container(
-              padding: const EdgeInsets.all(45),
+              padding: const EdgeInsets.all(20),
               child: const Align(
                 alignment: Alignment.topLeft,
                 child: Text('Three',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
               color: const Color.fromARGB(255, 81, 177, 167),
             ),
             Container(
-              padding: const EdgeInsets.all(45),
+              padding: const EdgeInsets.all(20),
               child: const Align(
                 alignment: Alignment.topLeft,
                 child: Text('Four',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
@@ -54,7 +54,7 @@ class MyApp extends StatelessWidget {
               color: const Color.fromARGB(255, 74, 214, 195),
             ),
             Container(
-              padding: const EdgeInsets.all(45),
+              padding: const EdgeInsets.all(20),
               child: const Align(
                 alignment: Alignment.topLeft,
                 child: Text('Five',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
@@ -62,7 +62,7 @@ class MyApp extends StatelessWidget {
               color: const Color.fromARGB(255, 33, 118, 139),
             ),
             Container(
-              padding: const EdgeInsets.all(45),
+              padding: const EdgeInsets.all(20),
               child: const Align(
                 alignment: Alignment.topLeft,
                 child: Text('Six',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
@@ -70,7 +70,7 @@ class MyApp extends StatelessWidget {
               color: const Color.fromARGB(255, 11, 100, 116),
             ),
             Container(
-              padding: const EdgeInsets.all(45),
+              padding: const EdgeInsets.all(20),
               child: const Align(
                 alignment: Alignment.topLeft,
                 child: Text('Seven',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
@@ -78,7 +78,7 @@ class MyApp extends StatelessWidget {
               color: const Color.fromARGB(255, 10, 99, 102),
             ),
             Container(
-              padding: const EdgeInsets.all(45),
+              padding: const EdgeInsets.all(20),
               child: const Align(
                 alignment: Alignment.topLeft,
                 child: Text('Eight',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
@@ -86,7 +86,7 @@ class MyApp extends StatelessWidget {
               color: const Color.fromARGB(255, 4, 104, 95),
             ),
             Container(
-              padding: const EdgeInsets.all(45),
+              padding: const EdgeInsets.all(20),
               child: const Align(
                 alignment: Alignment.topLeft,
                 child: Text('Nine',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
@@ -94,7 +94,7 @@ class MyApp extends StatelessWidget {
               color: const Color.fromARGB(255, 7, 63, 54),
             ),
             Container(
-              padding: const EdgeInsets.all(45),
+              padding: const EdgeInsets.all(20),
               child: const Align(
                 alignment: Alignment.topLeft,
                 child: Text('Ten',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
@@ -102,7 +102,7 @@ class MyApp extends StatelessWidget {
               color: const Color.fromARGB(255, 185, 238, 235),
             ),
             Container(
-              padding: const EdgeInsets.all(45),
+              padding: const EdgeInsets.all(20),
               child: const Align(
                 alignment: Alignment.topLeft,
                 child: Text('Eleven',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
@@ -110,7 +110,7 @@ class MyApp extends StatelessWidget {
               color: const Color.fromARGB(255, 137, 240, 223),
             ),
             Container(
-              padding: const EdgeInsets.all(45),
+              padding: const EdgeInsets.all(20),
               child: const Align(
                 alignment: Alignment.topLeft,
                 child: Text('Tweleve',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
